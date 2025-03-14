0c45778b52947708352aa060f6561349086c26d2
radhitya <radhitya@temanbsd.com>
2025-03-14 14:37:31 +0700

    aku commit

--- main.c
+++ main.c
@@ -11,44 +11,101 @@
 
 #define BUFFER_SIZE 1024
 
-pa_context *context;
-pa_mainloop *mainloop;
-pa_mainloop_api *mainloop_api;
-pa_operation *operation;
+static pa_context *context;
+static pa_mainloop *mainloop;
+static pa_mainloop_api *mainloop_api;
+static pa_operation *operation;
+static int ready = 0;
 
+static void
+context_state_callback_status(pa_context *context, void *userdata)
+{
+        int *pa_ready_status = userdata;
 
-int 
-main(int argc, char *argv[])
+        switch (pa_context_get_state(context)) {
+        case PA_CONTEXT_READY:
+                *pa_ready_status = 1; 
+                printf("Pulseaudio connected :)\n");
+                break;
+        case PA_CONTEXT_FAILED:
+        case PA_CONTEXT_TERMINATED:
+                *pa_ready_status = 2; 
+                break;
+        case PA_CONTEXT_UNCONNECTED:
+        case PA_CONTEXT_CONNECTING:
+        case PA_CONTEXT_AUTHORIZING:
+        case PA_CONTEXT_SETTING_NAME:
+        default:
+                break; 
+        }
+}
+
+void
+destroy(void)
+{
+	if ( context != NULL ) {
+	pa_context_unref(context);
+	}
+	
+	if ( mainloop != NULL ) {
+        pa_mainloop_free(mainloop);
+	}
+}
+
+int
+setup_server(void)
 {
 	mainloop = pa_mainloop_new();
-	if (mainloop = NULL)
+	if (mainloop == NULL)
 	{
 		printf("mainloop failed :(\n");
 		return 1;
 	}
 
 	mainloop_api = pa_mainloop_get_api(mainloop);
-	if (mainloop_api = NULL)
+	if (mainloop_api == NULL)
 	{
 		printf("mainloop_api failed :(\n");
 		return 1;
 	}
 
 	context = pa_context_new(mainloop_api, "cmix");
-	if (context = NULL)
+	if (context == NULL)
 	{
 		printf("context failed :(\n");
 		return 1;
 	}
 
-	if(pa_context_connect(context,
-			      NULL,
-			      PA_CONTEXT_NOAUTOSPAWN, 
-			      NULL) < 0)
+	pa_context_set_state_callback(context,
+				      context_state_callback_status,
+				      &ready);
+
+
+	if (pa_context_connect(context,
+			       NULL,
+			       0,
+			       NULL) < 0)
 	{
 		printf("pa_context_connect() failed :(");
 		return 1;
 	}
 
-	return 0;
+	pa_mainloop_run(mainloop, NULL);
+
+}
+
+int 
+main(int argc, char *argv[])
+{
+
+	setup_server();
+
+
+	if (ready == 2) {
+                printf("connection failed :(\n");
+        }
+
+	destroy();
+
+	return (ready == 1) ? 0 : 1;
 }
