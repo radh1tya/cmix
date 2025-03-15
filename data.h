9c5c61951273528125e6cfa8c35c3b6730ace590
radhitya <radhitya@temanbsd.com>
2025-03-15 19:45:19 +0700

    belum work

---
+++ data.h
@@ -0,0 +1,15 @@
+#ifndef STATUS_H
+#define STATUS_H
+
+#include <pulse/pulseaudio.h>
+
+typedef struct {
+    char name[512];
+    uint32_t index;
+    char description[256];
+    pa_cvolume volume;
+} pa_devicelist_t;
+
+extern pa_devicelist_t devicelist[10];
+
+#endif
---
+++ ui.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ncurses.h>
+
+#include "data.h"
+
+
+void
+interface(void)
+{
+	initscr();
+	printw("Sinks Volume[0]: %d\n", devicelist[0].volume.values[0]);
+	printw("Sinks Volume[1]: %d\n", devicelist[0].volume.values[1]);
+	refresh();
+	getch();
+	endwin();
+}
+
