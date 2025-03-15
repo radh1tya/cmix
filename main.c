30fe9b727b172ff7207d717dee2be2a2e4fd11e4
radhitya <radhitya@temanbsd.com>
2025-03-15 19:21:10 +0700

    info

--- main.c
+++ main.c
@@ -5,10 +5,6 @@
 #include <signal.h>
 #include <assert.h>
 #include <pulse/pulseaudio.h>
-#include <pulse/rtclock.h>
-#include <pulse/simple.h>
-#include <pulse/error.h>
-#include <pulse/introspect.h>
 
 #define BUFFER_SIZE 1024
 #define MAX_SINKS 10
@@ -24,6 +20,7 @@ typedef struct pa_devicelist {
     char name[512];
     uint32_t index;
     char description[256];
+    pa_cvolume volume;
 } pa_devicelist_t;
 
 static void
@@ -44,10 +41,10 @@ context_state_callback_status(pa_context *context, void *userdata)
 	*pa_ready_status = 2;
         break;
     default:
-	printf("pa_context_get_state: %d\n", pa_context_get_state(context)); 
+	//printf("pa_context_get_state: %d\n", pa_context_get_state(context)); 
         break;
     }
-    printf("Current ready: %d\n", *pa_ready_status);
+    //printf("Current ready: %d\n", *pa_ready_status);
 }
 
 static void
@@ -74,6 +71,7 @@ sinklist_callback(pa_context *context,
     strncpy(devicelist[sink_count].name, sink_info->name, 511);
     devicelist[sink_count].index = sink_info->index;
     strncpy(devicelist[sink_count].description, sink_info->description, 255);
+    devicelist[sink_count].volume = sink_info->volume;
     sink_count++;
 }
 
@@ -113,86 +111,95 @@ get_sinks()
     }
 
     pa_operation_unref(operation);
-    printf("Sinks found: %d\n", sink_count);
+    
+    for (int i = 0; i < sink_count; i++) {
+	    printf("Sinks found: %d\n", sink_count);
+
+	    printf("Sinks Name: %s\n", devicelist[i].name);
+	    printf("Sinks Index: %d\n", devicelist[i].index);
+	    printf("Sinks Volume[0]: %d\n", devicelist[i].volume.values[0]);
+	    printf("Sinks Volume[1]: %d\n", devicelist[i].volume.values[1]);
+    }
+    
     return 0;
 }
 
-void
+	void
 destroy(void)
 {
-    if (context != NULL) {
-        pa_context_unref(context);
-    }
-    if (mainloop != NULL) {
-        pa_mainloop_free(mainloop);
-    }
+	if (context != NULL) {
+		pa_context_unref(context);
+	}
+	if (mainloop != NULL) {
+		pa_mainloop_free(mainloop);
+	}
 }
 
-int
+	int
 setup_server(void)
 {
-    mainloop = pa_mainloop_new();
-    if (mainloop == NULL) {
-        printf("Mainloop failed :(\n");
-        return 1;
-    }
+	mainloop = pa_mainloop_new();
+	if (mainloop == NULL) {
+		printf("Mainloop failed :(\n");
+		return 1;
+	}
 
-    mainloop_api = pa_mainloop_get_api(mainloop);
-    if (mainloop_api == NULL) {
-        printf("Mainloop API failed :(\n");
-        pa_mainloop_free(mainloop);
-        return 1;
-    }
+	mainloop_api = pa_mainloop_get_api(mainloop);
+	if (mainloop_api == NULL) {
+		printf("Mainloop API failed :(\n");
+		pa_mainloop_free(mainloop);
+		return 1;
+	}
 
-    context = pa_context_new(mainloop_api, "cmix");
-    if (context == NULL) {
-        printf("Context failed :(\n");
-        destroy();
-        return 1;
-    }
+	context = pa_context_new(mainloop_api, "cmix");
+	if (context == NULL) {
+		printf("Context failed :(\n");
+		destroy();
+		return 1;
+	}
 
-    pa_context_set_state_callback(context, 
-				  context_state_callback_status, 
-				  &ready);
+	pa_context_set_state_callback(context, 
+				      context_state_callback_status, 
+				      &ready);
 
-    if (pa_context_connect(context, NULL, 0, NULL) < 0) {
-        printf("Context connect failed :(\n");
-        destroy();
-        return 1;
-    }
+	if (pa_context_connect(context, NULL, 0, NULL) < 0) {
+		printf("Context connect failed :(\n");
+		destroy();
+		return 1;
+	}
 
-    while ( ready == 0 ) {
-	    pa_mainloop_iterate(mainloop, 1, NULL);
-	    enum pa_context_state state = pa_context_get_state(context);
-	    if (state == PA_CONTEXT_FAILED || state == PA_CONTEXT_TERMINATED)
-	    {
-		    printf("connection failed early :(\n");
-		    return 1;
-	    }
-    }
-    return (ready == 1) ? 0 : 1;
+	while ( ready == 0 ) {
+		pa_mainloop_iterate(mainloop, 1, NULL);
+		enum pa_context_state state = pa_context_get_state(context);
+		if (state == PA_CONTEXT_FAILED || state == PA_CONTEXT_TERMINATED)
+		{
+			printf("connection failed early :(\n");
+			return 1;
+		}
+	}
+	return (ready == 1) ? 0 : 1;
 }
 
-int
+	int
 main(int argc, char *argv[])
 {
-    if (setup_server() == 1) {
-        printf("Setup server failed :(\n");
-        return 1;
-    }
+	if (setup_server() == 1) {
+		printf("Setup server failed :(\n");
+		return 1;
+	}
 
-    if (ready != 1) {
-        printf("Connection failed :(\n");
-        destroy();
-        return 1;
-    }
+	if (ready != 1) {
+		printf("Connection failed :(\n");
+		destroy();
+		return 1;
+	}
 
-    if (get_sinks() == 1) {
-        printf("Failed to get sinks :(\n");
-        destroy();
-        return 1;
-    }
+	if (get_sinks() == 1) {
+		printf("Failed to get sinks :(\n");
+		destroy();
+		return 1;
+	}
 
-    destroy();
-    return 0;
+	destroy();
+	return 0;
 }
